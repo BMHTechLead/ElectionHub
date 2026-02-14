af63bfbfa645d4a4819093bae237b93cb81770fb
BMHTechLead <bmhtechlead@gmail.com>
2026-02-14 03:22:38 +0300

    Initial ElectionHub production setup

--- electionhub/settings.py
+++ electionhub/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s1dv&%qo#u*%263-@&@(c6inkoz@3=dymbr0meta-7e09-y2%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["187.77.75.145","localhost",
+    "127.0.0.1",]
 
 
 # Application definition
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'electionhub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'electionhub_db',
+        'USER': 'electionhub_user',
+        'PASSWORD': 'TechLeadIQ@214!!!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -117,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "election_list"
